92e7479c792d039c0b813100148d74e3f14fb82f
tzcoDX <muramatu.kosuke.0610@gmail.com>
2021-01-07 01:12:37 +0900

    💄 ざっくりレイアウト

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core";
+
+import { CsvInput } from "../../components/Home";
 
 const Home: React.FC = () => {
-  return <div>fuga</div>;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth={false}>
+      <CsvInput />
+    </Container>
+  );
 };
 
 export default Home;
+
+const useStyles = makeStyles((theme) => ({}));
